f6f5634c87baef29a6d22d1e4c8bc4195abd0534
Adel <musin.f.mail@gmail.com>
2023-06-18 16:59:53 +0300

    chore(colors.dart) primary colors for the application

---
+++ lib/config/colors.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const backgroundBlack = Color.fromRGBO(31, 41, 51, 1);
+  static const white = Color.fromRGBO(245, 247, 250, 1);
+  // static final halfWhite = Color.fromRGBO(245, 247, 250, 0.5);
+  // static final orange = Color.fromRGBO(243, 86, 39, 1);
+  // static final green = Color.fromRGBO(87, 174, 91, 1);
+  // static final darkGrey = Color.fromRGBO(71, 75, 80, 1);
+  // static final lightGrey = Color.fromRGBO(228, 231, 235, 1);
+  // static final slateGrey = Color.fromRGBO(82, 96, 109, 1);
+  // static final smokyBlue = Color.fromRGBO(50, 63, 75, 1);
+  // static final darkBlue = Color.fromRGBO(62, 76, 89, 1);
+  // static final greenComics = Color.fromRGBO(101, 189, 94, 1);
+  // static final blueComics = Color.fromRGBO(48, 130, 196, 1);
+  // static final graphite = Color.fromRGBO(97, 110, 124, 1);
+  // static final taupe = Color.fromRGBO(119, 120, 123, 1);
+  // static final closeButton = Color.fromRGBO(247, 248, 253, 1);
+  // static final comics_analytics_and_design = Color.fromRGBO(250, 181, 160, 1);
+  // static final comics_design = Color.fromRGBO(159, 171, 211, 1);
+  // static final comics_development = Color.fromRGBO(178, 222, 174, 1);
+  // static final comics_testing = Color.fromRGBO(253, 206, 174, 1);
+  // static final comics_administration = Color.fromRGBO(202, 174, 210, 1);
+  // static final comics_management = Color.fromRGBO(127, 211, 200, 1);
+  // static final comics_staff = Color.fromRGBO(170, 193, 228, 1);
+  // static final comics_students = Color.fromRGBO(170, 193, 228, 1);
+  // static final promo_code_item = Color.fromRGBO(62, 76, 89, 1);
+  // static final fCBD2D9 = Color.fromRGBO(203, 210, 217, 1);
+  // static final f616E7C = Color.fromRGBO(97, 110, 124, 1);
+  // static final silver = Color.fromRGBO(198, 198, 198, 1);
+}
