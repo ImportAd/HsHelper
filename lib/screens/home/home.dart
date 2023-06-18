1a36b16d5e5241fbeea6089d9cdfad8ae9f15492
ImportAd <musin.f.mail@gmail.com>
2023-06-18 21:33:00 +0300

    chore(home.dart) change the home screen theme

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,23 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-//import 'package:hs_helper/screens/home/home_controller.dart';
+import 'package:hs_helper/config/colors.dart';
+import 'package:hs_helper/screens/home/home_controller.dart';
 import 'package:hs_helper/screens/secrets/secrets.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
   @override
   Widget build(context) {
-    //final Controller c = Get.put(Controller());
+    final TextTheme textTheme = Theme.of(context).textTheme;
+    Get.put(Controller());
 
     return Scaffold(
-      appBar: AppBar(title: const Text("Home screen"),
-              ),
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            "Home screen",
+            style: textTheme.titleMedium,
+          ),
+        ),
+        backgroundColor: AppColors.backgroundBlack,
+      ),
       body: Center(
-        child: ElevatedButton(
-              child: const Text("Secrets"), 
-              onPressed: () => Get.to(Other()),
-              ),
+        child: TextButton(
+            child: Text(
+              "Secrets",
+              style: textTheme.headlineLarge,
             ),
-          );
+            onPressed: () => Get.to(Other())),
+      ),
+    );
   }
 }
