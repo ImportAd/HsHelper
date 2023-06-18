45a837589f95d246f7c1d6c42fbf2b94cd8bc5ab
Adel <musin.f.mail@gmail.com>
2023-06-18 17:03:14 +0300

    chore(theme.dart) initialization of the main widget

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:hs_helper/screens/home/home.dart';
+import 'package:hs_helper/config/theme.dart';
+
+
+void main() => runApp(GetMaterialApp(home: const App(),
+theme: theme(),));
