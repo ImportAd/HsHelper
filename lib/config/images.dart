2c5f4e36d7904290ba32d47ce7ce5c1cd9452781
ImportAd <musin.f.mail@gmail.com>
2023-06-19 15:14:39 +0300

    feature(images) links to images are in a separate file

---
+++ lib/config/images.dart
@@ -0,0 +1,49 @@
+abstract class AppImage {
+  static const counterspell = "assets/secrets/mage/counterspell.png";
+  static const explosiveRunes = "assets/secrets/mage/explosive_runes.png";
+  static const iceBarrier = "assets/secrets/mage/ice_barrier.png";
+  static const objection = "assets/secrets/mage/objection.png";
+  static const vengefulVisage = "assets/secrets/mage/vengeful_visage.png";
+  static const secretsMageList = [
+    counterspell,
+    explosiveRunes,
+    iceBarrier,
+    objection,
+    vengefulVisage,
+  ];
+  static const catTrick = "assets/secrets/hunter/cat_trick.png";
+  static const emergencyManeuvers =
+      "assets/secrets/hunter/emergency_maneuvers.png";
+  static const explosiveTrap = "assets/secrets/hunter/explosive_trap.png";
+  static const cafreezingTraptTrick = "assets/secrets/hunter/freezing_trap.png";
+  static const hiddenMeaning = "assets/secrets/hunter/hidden_meaning.png";
+  static const motionDenied = "assets/secrets/hunter/motion_denied.png";
+  static const wanderingMonster = "assets/secrets/hunter/wandering_monster.png";
+  static const zombeeees = "assets/secrets/hunter/zombeeees.png";
+  static const secretsHunterList = [
+    catTrick,
+    emergencyManeuvers,
+    explosiveTrap,
+    cafreezingTraptTrick,
+    hiddenMeaning,
+    motionDenied,
+    wanderingMonster,
+    zombeeees,
+  ];
+  static const ambush = "assets/secrets/rogue/ambush.png";
+  static const cheatDeath = "assets/secrets/rogue/cheat_death.png";
+  static const doubleCross = "assets/secrets/rogue/double_cross.png";
+  static const kidnap = "assets/secrets/rogue/kidnap.png";
+  static const plagiarize = "assets/secrets/rogue/plagiarize.png";
+  static const prejury = "assets/secrets/rogue/prejury.png";
+  static const stickySituation = "assets/secrets/rogue/sticky_situation.png";
+  static const secretsRogueList = [
+    ambush,
+    cheatDeath,
+    doubleCross,
+    kidnap,
+    plagiarize,
+    prejury,
+    stickySituation,
+  ];
+}
