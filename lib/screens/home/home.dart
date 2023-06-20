77a99d598b384e1fae6de8bf31eccb4662ae7164
ImportAd <musin.f.mail@gmail.com>
2023-06-20 20:24:14 +0300

    feature(home) changed the structure of the main screen

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,16 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:hs_helper/config/colors.dart';
-import 'package:hs_helper/screens/home/home_controller.dart';
-import 'package:hs_helper/screens/secrets/secrets.dart';
+import 'package:hs_helper/screens/mage/mage.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
   @override
   Widget build(context) {
     final TextTheme textTheme = Theme.of(context).textTheme;
-    Get.put(Controller());
-
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -21,13 +18,90 @@ class App extends StatelessWidget {
         elevation: 0,
         centerTitle: true,
       ),
-      body: Center(
-        child: TextButton(
-            child: Text(
-              "Secrets",
-              style: textTheme.headlineLarge,
-            ),
-            onPressed: () => Get.to(const SecretsScreen())),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 40.0, bottom: 40),
+        child: Center(
+          child: Column(
+            children: [
+              TextButton(
+                  child: Text(
+                    "Death Knight",
+                    style: textTheme.headlineLarge,
+                  ),
+                  onPressed: () => Get.to(const MageScreen())),
+              const SizedBox(height: 16),
+              TextButton(
+                  child: Text(
+                    "Demon Hunter",
+                    style: textTheme.headlineLarge,
+                  ),
+                  onPressed: () => Get.to(const MageScreen())),
+              const SizedBox(height: 16),
+              TextButton(
+                  child: Text(
+                    "Druid",
+                    style: textTheme.headlineLarge,
+                  ),
+                  onPressed: () => Get.to(const MageScreen())),
+              const SizedBox(height: 16),
+              TextButton(
+                  child: Text(
+                    "Hunter",
+                    style: textTheme.headlineLarge,
+                  ),
+                  onPressed: () => Get.to(const MageScreen())),
+              const SizedBox(height: 16),
+              TextButton(
+                  child: Text(
+                    "Mage",
+                    style: textTheme.headlineLarge,
+                  ),
+                  onPressed: () => Get.to(const MageScreen())),
+              const SizedBox(height: 16),
+              TextButton(
+                  child: Text(
+                    "Paladin",
+                    style: textTheme.headlineLarge,
+                  ),
+                  onPressed: () => Get.to(const MageScreen())),
+              const SizedBox(height: 16),
+              TextButton(
+                  child: Text(
+                    "Priest",
+                    style: textTheme.headlineLarge,
+                  ),
+                  onPressed: () => Get.to(const MageScreen())),
+              const SizedBox(height: 16),
+              TextButton(
+                  child: Text(
+                    "Rogue",
+                    style: textTheme.headlineLarge,
+                  ),
+                  onPressed: () => Get.to(const MageScreen())),
+              const SizedBox(height: 16),
+              TextButton(
+                  child: Text(
+                    "Shaman",
+                    style: textTheme.headlineLarge,
+                  ),
+                  onPressed: () => Get.to(const MageScreen())),
+              const SizedBox(height: 16),
+              TextButton(
+                  child: Text(
+                    "Warlock",
+                    style: textTheme.headlineLarge,
+                  ),
+                  onPressed: () => Get.to(const MageScreen())),
+              const SizedBox(height: 16),
+              TextButton(
+                  child: Text(
+                    "Warrior",
+                    style: textTheme.headlineLarge,
+                  ),
+                  onPressed: () => Get.to(const MageScreen())),
+            ],
+          ),
+        ),
       ),
     );
   }
