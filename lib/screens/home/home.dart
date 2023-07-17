68fc0b5df632b7fdf0468016823d25c4d8ecb1a9
ImportAd <musin.f.mail@gmail.com>
2023-07-17 14:14:57 +0300

    feature (home.dart) added content generation

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,7 +1,7 @@
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:flutter/material.dart';
 import 'package:hs_helper/config/colors.dart';
-import 'package:hs_helper/screens/mage/mage.dart';
+import 'package:hs_helper/config/constants.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -19,86 +19,27 @@ class App extends StatelessWidget {
         centerTitle: true,
       ),
       body: Padding(
-        padding: const EdgeInsets.only(top: 40.0, bottom: 40),
+        padding: const EdgeInsets.only(top: 40, bottom: 40),
         child: Center(
           child: Column(
             children: [
-              TextButton(
-                  child: Text(
-                    "Death Knight",
-                    style: textTheme.headlineLarge,
-                  ),
-                  onPressed: () => Get.to(const MageScreen())),
-              const SizedBox(height: 16),
-              TextButton(
-                  child: Text(
-                    "Demon Hunter",
-                    style: textTheme.headlineLarge,
-                  ),
-                  onPressed: () => Get.to(const MageScreen())),
-              const SizedBox(height: 16),
-              TextButton(
-                  child: Text(
-                    "Druid",
-                    style: textTheme.headlineLarge,
-                  ),
-                  onPressed: () => Get.to(const MageScreen())),
-              const SizedBox(height: 16),
-              TextButton(
-                  child: Text(
-                    "Hunter",
-                    style: textTheme.headlineLarge,
-                  ),
-                  onPressed: () => Get.to(const MageScreen())),
-              const SizedBox(height: 16),
-              TextButton(
-                  child: Text(
-                    "Mage",
-                    style: textTheme.headlineLarge,
-                  ),
-                  onPressed: () => Get.to(const MageScreen())),
-              const SizedBox(height: 16),
-              TextButton(
-                  child: Text(
-                    "Paladin",
-                    style: textTheme.headlineLarge,
-                  ),
-                  onPressed: () => Get.to(const MageScreen())),
-              const SizedBox(height: 16),
-              TextButton(
-                  child: Text(
-                    "Priest",
-                    style: textTheme.headlineLarge,
-                  ),
-                  onPressed: () => Get.to(const MageScreen())),
-              const SizedBox(height: 16),
-              TextButton(
-                  child: Text(
-                    "Rogue",
-                    style: textTheme.headlineLarge,
-                  ),
-                  onPressed: () => Get.to(const MageScreen())),
-              const SizedBox(height: 16),
-              TextButton(
-                  child: Text(
-                    "Shaman",
-                    style: textTheme.headlineLarge,
-                  ),
-                  onPressed: () => Get.to(const MageScreen())),
-              const SizedBox(height: 16),
-              TextButton(
-                  child: Text(
-                    "Warlock",
-                    style: textTheme.headlineLarge,
-                  ),
-                  onPressed: () => Get.to(const MageScreen())),
-              const SizedBox(height: 16),
-              TextButton(
-                  child: Text(
-                    "Warrior",
-                    style: textTheme.headlineLarge,
-                  ),
-                  onPressed: () => Get.to(const MageScreen())),
+              Expanded(
+                child: ListView.builder(
+                    itemCount: AppConstants.characterSheet.length,
+                    physics: const BouncingScrollPhysics(),
+                    itemBuilder: (BuildContext context, index) {
+                      return Container(
+                        padding: const EdgeInsets.only(bottom: 16),
+                        child: TextButton(
+                            child: Text(
+                              AppConstants.characterSheet[index],
+                              style: textTheme.headlineLarge,
+                            ),
+                            onPressed: () => Get.to(
+                                AppConstants.characterScreenSheet[index])),
+                      );
+                    }),
+              )
             ],
           ),
         ),
