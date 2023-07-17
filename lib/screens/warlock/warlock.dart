4fa22f6ac59e5a694ad6db8c144c2907a3fb71e2
ImportAd <musin.f.mail@gmail.com>
2023-07-17 14:12:57 +0300

    chore (warlock.dart) adding a separate screen for a given class

---
+++ lib/screens/warlock/warlock.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:hs_helper/config/colors.dart';
+import 'package:hs_helper/screens/mage/secrets.dart';
+
+class WarlockScreen extends StatelessWidget {
+  const WarlockScreen({super.key});
+
+  @override
+  Widget build(context) {
+    final TextTheme textTheme = Theme.of(context).textTheme;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Warlock screen",
+          style: textTheme.titleLarge,
+        ),
+        backgroundColor: AppColors.backgroundBlack,
+        elevation: 0,
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 40.0, bottom: 40),
+        child: Center(
+          child: Column(children: [
+            TextButton(
+                child: Text(
+                  "Secrets",
+                  style: textTheme.headlineLarge,
+                ),
+                onPressed: () => Get.to(const MageSecretsScreen())),
+            const SizedBox(height: 16),
+            TextButton(
+                child: Text(
+                  "Decks",
+                  style: textTheme.headlineLarge,
+                ),
+                onPressed: () => Get.to(const MageSecretsScreen())),
+            const SizedBox(height: 16),
+            TextButton(
+                child: Text(
+                  "Deck Editing",
+                  style: textTheme.headlineLarge,
+                ),
+                onPressed: () => Get.to(const MageSecretsScreen())),
+          ]),
+        ),
+      ),
+    );
+  }
+}
