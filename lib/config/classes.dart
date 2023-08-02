9c924ac7606a0cd5fb03c7ad62e7034dbe65059d
ImportAd <musin.f.mail@gmail.com>
2023-08-02 14:28:17 +0300

    chore

---
+++ lib/config/classes.dart
@@ -0,0 +1,54 @@
+class DeathKnight {
+  static const String name = "Death Knight";
+  static const String title = "Death Knight Screen";
+}
+
+class DemonHunter {
+  static const String name = "Demon Hunter";
+  static const String title = "Demon Hunter Screen";
+}
+
+class Druid {
+  static const String name = "Druid";
+  static const String title = "Druid Screen";
+}
+
+class Hunter {
+  static const String name = "Hunter";
+  static const String title = "Hunter Screen";
+}
+
+class Mage {
+  static const String name = "Mage";
+  static const String title = "Mage Screen";
+}
+
+class Paladin {
+  static const String name = "Paladin";
+  static const String title = "Paladin Screen";
+}
+
+class Priest {
+  static const String name = "Priest";
+  static const String title = "Priest Screen";
+}
+
+class Rogue {
+  static const String name = "Rogue";
+  static const String title = "Rogue Screen";
+}
+
+class Shaman {
+  static const String name = "Shaman";
+  static const String title = "Shaman Screen";
+}
+
+class Warlock {
+  static const String name = "Warlock";
+  static const String title = "Warlock Screen";
+}
+
+class Warrior {
+  static const String name = "Warrior";
+  static const String title = "Warrior Screen";
+}
